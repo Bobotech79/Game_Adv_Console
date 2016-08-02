145609ae519d9592828e440ecb473ff122e68f22
Bobotech79 <support.tony@gmail.com>
2016-08-02 23:04:04 +0200

    Test Console

---
+++ Adv_Game_Console/Adv_Game_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adv_Game_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Adventure Games.... ");
+        }
+    }
+}
